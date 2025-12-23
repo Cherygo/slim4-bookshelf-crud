873f67ffa4a9055592b8f328d9e3bcf7b32bec67
Cherygo <olexdzhy@gmail.com>
2025-12-23 19:19:03 +0200

    Add index.php + bootstrap assets

---
+++ public/index.php
@@ -0,0 +1,133 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\BookController as AdminBookController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BookmarkController;
+use App\Middlewares\AdminMiddleware;
+use App\Repositories\BookRepository;
+use DI\Container;
+use Slim\Exception\HttpForbiddenException;
+use Slim\Factory\AppFactory;
+use Slim\Flash\Messages;
+use Slim\Middleware\MethodOverrideMiddleware;
+use Slim\Psr7\Response;
+use Slim\Routing\RouteCollectorProxy;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
+
+session_start();
+
+$container = new Container();
+
+$container->set(Twig::class, function () {
+    return Twig::create(__DIR__  . '/../templates', ['cache' => false]);
+});
+
+$container->set('flash', function () {
+    return new Messages();
+});
+
+$container->set(PDO::class, function () {
+    try {
+        $host = '127.0.0.1';
+        $dbname = 'Bookshelf';
+        $user = 'Cherygo';
+        $pass = 'root';
+
+        $conn = new PDO("mysql:host=$host;port=3306;dbname=$dbname", $user, $pass);
+        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        return $conn;
+    } catch (PDOException $e) {
+        die("Database error " . $e->getMessage());
+//        return $e;
+    }
+});
+
+$app = AppFactory::createFromContainer($container);
+
+// flash messages
+$app->add(function ($request, $handler) use ($container) {
+    $container->get(Twig::class)->getEnvironment()->addGlobal('flash', $container->get('flash')->getMessages());
+    return $handler->handle($request);
+});
+
+// session
+$app->add(function ($request, $handler) use ($container) {
+   if(isset($_SESSION)) {
+       $this->get(Twig::class)->getEnvironment()->addGlobal('session', $_SESSION);
+   }
+   return $handler->handle($request);
+});
+
+$app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
+$app->addRoutingMiddleware();
+$app->add(MethodOverrideMiddleware::class);
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware->setErrorHandler(
+    HttpForbiddenException::class,
+    function() use($container)
+    {
+        $twig = $container->get(Twig::class);
+        $response = new Response();
+        return $twig->render($response, "errors/403.twig")->withStatus(403);
+    }
+);
+
+
+// Routes
+$app->get('/', function ($request, $response) {
+    $view = $this->get(Twig::class);
+    $newestBooks = $this->get(BookRepository::class)->getLastBooks();
+    $totalBookCount = $this->get(BookRepository::class)->getBooksCount();
+
+    return $view->render($response, 'home.twig', [
+        'pageTitle' => 'Home Page',
+        'newest_books' => $newestBooks,
+        'total_book_count' => $totalBookCount,
+    ]);
+});
+
+// AUTH
+$app->get('/register', AuthController::class . ':registerPage');
+
+$app->post('/register', AuthController::class . ':store');
+
+
+$app->get('/login', AuthController::class . ':loginPage');
+
+$app->post('/login', AuthController::class . ':login');
+
+
+$app->post('/logout', AuthController::class . ':logout')->setName('logout');
+
+// Catalog
+$app->get('/catalog', BookController::class . ':catalogPage');
+
+// Bookmark
+$app->get('/bookmarks', BookmarkController::class . ':myBookmarksPage')
+    ->setName('bookmarks.index');
+
+$app->post('/bookmarks', BookmarkController::class . ':addBookmark')
+    ->setName('bookmarks.store');
+
+$app->delete('/my_bookmarks/{id}', BookmarkController::class . ':deleteBookmark')
+    ->setName("bookmarks.destroy");
+
+
+// Admin
+$app->group('/admin', function (RouteCollectorProxy $group) {
+    $group->get('', AdminController::class);
+    $group->get('/books', AdminBookController::class . ':index');
+    $group->get('/books/create', AdminBookController::class . ':create');
+
+})->add(new AdminMiddleware());
+
+$app->run();
