c1d228acf325b4b91e25c0eabe1fc53f4d076c24
Cherygo <olexdzhy@gmail.com>
2025-12-29 15:15:06 +0200

    Add create and store functions necessary for book creation

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\DTOs\BookDTO;
 use App\Http\Controllers\Controller;
 use App\Repositories\BookRepository;
 use App\Services\BookService;
 use Psr\Http\Message\ResponseInterface;
 use Slim\Flash\Messages;
+use Slim\Http\Response;
+use Slim\Http\ServerRequest as Request;
 use Slim\Views\Twig;
 
 class BookController extends Controller
@@ -15,6 +18,7 @@ class BookController extends Controller
         Messages $flash,
         Twig $twig,
         private readonly BookRepository $bookRepository,
+        private readonly BookService $bookService,
     ) {
         parent::__construct($flash, $twig);
     }
@@ -24,18 +28,31 @@ class BookController extends Controller
     {
         $allBooks = $this->bookRepository->getAllBooks();
 
-        return $this->render($response, 'admin/admin_books.twig', [
+        return $this->render($response, 'admin/books/index.twig', [
             'allBooks' => $allBooks,
         ]);
     }
 
-    public function create()
+    public function create($request, $response): ResponseInterface
     {
-
+        $response = $this->render($response, 'admin/books/create.twig', [
+            'errors' => $_SESSION['errors'] ?? [],
+        ]);
+        unset($_SESSION['errors']);
+        return $response;
     }
 
-    public function store()
+    public function store(Request $request, Response $response): ResponseInterface
     {
+        $params = (array)$request->getParsedBody();
+        $book = BookDTO::fromArray($params);
 
+        try {
+            $this->bookService->store($book);
+        } catch (\RuntimeException $e) {
+            return $response->withRedirect('/admin/books/create');
+        }
+
+        return $response->withRedirect('/admin/books');
     }
 }
