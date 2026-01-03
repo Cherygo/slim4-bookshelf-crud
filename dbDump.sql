61137d431f4fa6f42bfb11760c942bacc038a18c
Cherygo <olexdzhy@gmail.com>
2026-01-03 14:25:01 +0200

    Update dbDump to be PostgreSQL compatible

--- dbDump.sql
+++ dbDump.sql
@@ -1,27 +1,28 @@
-CREATE TABLE users (
-                        id SERIAL PRIMARY KEY,
-                        username VARCHAR(100) NOT NULL UNIQUE,
-                        email VARCHAR(255) NOT NULL UNIQUE,
-                        password VARCHAR(255) NOT NULL,
-                        role VARCHAR(255) NOT NULL DEFAULT 'user',
-                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE books (
-                       id SERIAL PRIMARY KEY,
-                       title VARCHAR(255) NOT NULL,
-                       author VARCHAR(255),
-                       url VARCHAR(10000),
-                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE bookmarked (
+    CREATE TABLE users (
                             id SERIAL PRIMARY KEY,
-                            user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-                            book_id INT NOT NULL REFERENCES books(id) ON DELETE CASCADE,
-                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                            UNIQUE (user_id, book_id)
-);
+                            username VARCHAR(100) NOT NULL UNIQUE,
+                            email VARCHAR(255) NOT NULL UNIQUE,
+                            password VARCHAR(255) NOT NULL,
+                            role VARCHAR(255) NOT NULL DEFAULT 'user',
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
 
-INSERT INTO users (username, email, password, role)
-VALUES ('root', 'root@gmail.com', '$2y$12$s5dQM8OY3f7EDd.lWtGCuexdA5o42lrd7h/7D/94mIzkel1vctbXm', 'admin')
+    CREATE TABLE books (
+                           id SERIAL PRIMARY KEY,
+                           title VARCHAR(255) NOT NULL,
+                           author VARCHAR(255),
+                           url VARCHAR(10000),
+                           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+    CREATE TABLE bookmarked (
+                                id SERIAL PRIMARY KEY,
+                                user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+                                book_id INT NOT NULL REFERENCES books(id) ON DELETE CASCADE,
+                                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                UNIQUE (user_id, book_id)
+    );
+
+
+    INSERT INTO users (username, email, password, role)
+    VALUES ('root', 'root@gmail.com', '$2y$12$s5dQM8OY3f7EDd.lWtGCuexdA5o42lrd7h/7D/94mIzkel1vctbXm', 'admin')
